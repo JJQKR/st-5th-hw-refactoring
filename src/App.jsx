68ee15ee156286e46e7e6d2ca016a73a3001b476
JJQKR <yjcoyjco@naver.com>
2024-05-27 18:36:25 +0900

    useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect, createContext, useContext } from "react";
+import React, { Children } from "react";
 import TextInput from "./components/TextInput";
 import TextList from "./components/TextList";
 import TextContextProvider from "./context/TextContextProvider";
+// src안에 context 폴더 만들고 TextContextProvider 컴포넌트 생성
+// text 관련, App.jsx 대신 저기서 관리하면
+// App.jsx가 더 가벼워지고 선택적 사용 가능
 
 function App() {
   return (
@@ -11,6 +14,12 @@ function App() {
         <TextInput />
         <TextList />
       </TextContextProvider>
+      {/* TextInput과 TextList가
+        TextContextProvider의 children props다
+        <TextContext.Provider value={{}}>
+          {Children}
+        </TextContext.Provider>
+        이렇게 감싸지고 있는 구조 */}
     </div>
   );
 }
--- src/context/TextContextProvider.jsx
+++ src/context/TextContextProvider.jsx
@@ -1,10 +1,9 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 const TextsContext = createContext(null);
-
-const onAddText = (text) => {
-  setTexts((prevTexts) => [...prevTexts, text]);
-};
+//함수 컴포넌트 외부에서 TextContext 생성
+//TextContextProvider 외부의 다른 컴포넌트에서 TextsContext를 사용하려면
+//앞에 export 키워드 필요
 
 const TextContextProvider = ({ children }) => {
   const [texts, setTexts] = useState(() =>
@@ -12,6 +11,10 @@ const TextContextProvider = ({ children }) => {
       ? JSON.parse(localStorage.getItem("texts"))
       : []
   );
+
+  const onAddText = (text) => {
+    setTexts((prevTexts) => [...prevTexts, text]);
+  };
 
   useEffect(() => {
     localStorage.setItem("texts", JSON.stringify(texts));
@@ -23,8 +26,10 @@ const TextContextProvider = ({ children }) => {
     </TextsContext.Provider>
   );
 };
-
-// 위 과정을 App.jsx에서 써도 되지만 그러면 App.jsx가 무거워지고
-// 선택적으로 Context를 사용하기 불편함!
+//TextInput과 TextList는 TextContextProvider의 직접적인 자식이 아니고
+//그런데 {children}부분에 렌더링 되므로
+//texts, setTexts, onAddText를 직접 import할 필요 없음
+//대신 useContext 훅을 사용하여
+//TextsContext에서 필요한 값들을 가져올 수 있다.
 
 export default TextContextProvider;
